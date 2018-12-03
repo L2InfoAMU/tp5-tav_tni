c6da9e63fab1bc811412cf171b7dccd239cdfab6
t17001504 <charles.tavenard@etu.univ-amu.fr>
2018-12-03 16:02:06 +0100

    modification des classes RasterFlagFactory et RasterUniformImageFactory pour le cas où on a PaletteRasterImage au lieu de BruteRasterImage

--- src/main/java/image/RasterFlagFactory.java
+++ src/main/java/image/RasterFlagFactory.java
@@ -22,16 +22,16 @@ public class RasterFlagFactory implements ImageFactory {
 
     @Override
     public Image makeImage() {
-        Color[][] colors = new Color[width][height];
+        Color[][] pixels = new Color[width][height];
 
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
-                colors[x][y] = (x < width / 3) ? leftColor : ((x > 2 * width / 3) ? rightColor : middleColor);
+                pixels[x][y] = (x < width / 3) ? leftColor : ((x > 2 * width / 3) ? rightColor : middleColor);
             }
         }
         switch (rasterImageType){
-            case BRUTE:
-                return new BruteRasterImage(colors);
+            case PALETTE:
+                return new PaletteRasterImage(pixels);
             default:
                 throw new NotSupportedException(rasterImageType + " is not supported");
         }
