679f5dc0a8957e288ae3e90b4297f9ad09648aa0
t17001504 <charles.tavenard@etu.univ-amu.fr>
2018-12-03 15:58:33 +0100

    creation de la classe PaletteRasterImage qui ressemble fortement à la classe BruteRasterImage, une simplification est possible à faire dans le futur

--- src/main/java/image/PaletteRasterImage.java
+++ src/main/java/image/PaletteRasterImage.java
@@ -1,32 +1,78 @@
 package image;
 
 import javafx.scene.paint.Color;
+import util.Matrices;
 
 public class PaletteRasterImage implements Image{
-
+    int width;
+    int height;
+    Color[][] pixels;
 
     public  PaletteRasterImage(Color color, int width, int height){
+        this.width = width;
+        this.height = height;
+        createRepresentation();
+        setPixelsColor(color);
 
     }
 
 
     public PaletteRasterImage(Color[][] pixels) {
+        Matrices.requiresNonZeroDimensions(pixels);
+        Matrices.requiresRectangularMatrix(pixels);
+        width = pixels.length;
+        height = pixels[0].length;
+
+        for (int i = 0; i < width; i++){
+            for( int j = 0; j < height; j++){
+                this.pixels[i][j] = pixels[i][j];
+            }
+        }
+
 
     }
 
 
     @Override
     public Color getPixelColor(int x, int y) {
-        return null;
+        return pixels[x][y];
     }
 
     @Override
     public int getWidth() {
-        return 0;
+        return width;
     }
 
     @Override
     public int getHeight() {
-        return 0;
+        return height;
+    }
+
+    public void setPixelsColor(Color color, int x, int y){
+        this.pixels[x][y] = color;
+    }
+
+    public void setPixelsColor(Color color) {
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++){
+                pixels[i][j] = color;
+            }
+        }
+    }
+
+    public void setPixelsColors(Color[][] colors) {
+        this.pixels = pixels.clone();
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void createRepresentation() {
+        pixels = new Color[getWidth()][getHeight()];
     }
 }
